29c3493deab44b5c7216e2f0016c1c8a41cb9d87
Erick Rezende <erickb.rezende@outlook.com>
2023-05-02 15:01:40 -0300

    Integration with Angular + Node.js + MongoDB

---
+++ frontend/src/app/services/food.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { sample_foods } from 'src/data';
+import { Food } from '../shared/models/Food';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FoodService {
+  constructor() {}
+  getAll(): Food[] {
+    return sample_foods;
+  }
+}
